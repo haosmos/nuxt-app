2cb7914798972d7f43023f0ceda14813ccd04a74
chaosmos <vitovich1987@gmail.com>
2023-06-29 22:24:18 +0300

    added chapter navigation

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,22 @@
+<template>
+  <iframe
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+    frameborder="0"
+    height="315"
+    title="Video player"
+    width="460"
+  ></iframe>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  videoId: {
+    type: Number,
+    required: true
+  }
+});
+</script>
+
+<style scoped></style>
--- pages/course.vue
+++ pages/course.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-      class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
+    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
   >
     <div class="prose mb-12">
       <h1>
@@ -10,18 +10,51 @@
         </span>
       </h1>
     </div>
-
+    
     <div class="flex flex-row justify-center flex-grow">
       <div
-          class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
+        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
       >
         <h3>Chapters</h3>
-        <!-- All the lessons for the course listed here -->
+        
+        <div
+          v-for="chapter in chapters"
+          :key="chapter.slug"
+          class="space-y-1 mb-4 flex flex-col"
+        >
+          <h4>{{ chapter.title }}</h4>
+          <NuxtLink
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            :class="{
+              'text-blue-500': lesson.path === $route.fullPath,
+              'text-gray-600': lesson.path !== $route.fullPath
+            }"
+            :to="lesson.path"
+            class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
+          >
+            <span class="text-gray-500"
+            >{{ index + 1 }}.</span
+            >
+            <span>{{ lesson.title }}</span>
+          </NuxtLink>
+        </div>
+        
       </div>
-
-      <div class="prose p-12 bg-red-200 rounded-md w-[65ch]">
+      
+      <div class="prose p-12 bg-white rounded-md w-[65ch]">
         <NuxtPage />
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+const { chapters } = useCourse();
+</script>
+
+<!--<style scoped>-->
+<!--.router-link-active {-->
+<!--  @apply text-blue-500;-->
+<!--}-->
+<!--</style>-->
