4e284a0319c2a3145889cf19690c53a339c994ac
chaosmos <vitovich1987@gmail.com>
2023-07-02 00:36:52 +0300

    added route validation and error handling

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,31 +1,70 @@
 <template>
   <div>
     <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
-      Lesson {{ chapter.number }} - {{ lesson.number}}
+      Lesson {{ chapter.number }} - {{ lesson.number }}
     </p>
-
     <h2 class="my-0">{{ lesson.title }}</h2>
     <div class="flex space-x-4 mt-2 mb-8">
-      <NuxtLink v-if="lesson.sourceUrl" :to="lesson.sourceUrl" class="font-normal text-md text-gray-500">
+      <NuxtLink
+        v-if="lesson.sourceUrl"
+        :to="lesson.sourceUrl"
+        class="font-normal text-md text-gray-500"
+      >
         Download Source Code
       </NuxtLink>
-      <NuxtLink v-if="lesson.downloadUrl" :to="lesson.downloadUrl" class="font-normal text-md text-gray-500">
+      <NuxtLink
+        v-if="lesson.downloadUrl"
+        :to="lesson.downloadUrl"
+        class="font-normal text-md text-gray-500"
+      >
         Download Video
       </NuxtLink>
     </div>
-    <VideoPlayer v-if="lesson.videoId" :videoId="lesson.videoId" />
+    <VideoPlayer
+      v-if="lesson.videoId"
+      :videoId="lesson.videoId"
+    />
     <p>{{ lesson.text }}</p>
-
-    <LessonCompleteButton :model-value="isLessonComplete" @update:model-value="toggleComplete" />
-
+    <LessonCompleteButton
+      :model-value="isLessonComplete"
+      @update:model-value="toggleComplete"
+    />
   </div>
 </template>
 
 <script setup>
-import { useLocalStorage } from '@vueuse/core';
-
 const course = useCourse();
 const route = useRoute();
+
+definePageMeta({
+  validate({ params }) {
+    const course = useCourse();
+    
+    const chapter = course.chapters.find(
+      (chapter) => chapter.slug === params.chapterSlug
+    );
+    
+    if (!chapter) {
+      return createError({
+        statusCode: 404,
+        message: 'Chapter not found',
+      });
+    }
+    
+    const lesson = chapter.lessons.find(
+      (lesson) => lesson.slug === params.lessonSlug
+    );
+    
+    if (!lesson) {
+      return createError({
+        statusCode: 404,
+        message: 'Lesson not found',
+      });
+    }
+    
+    return true;
+  },
+});
 
 const chapter = computed(() => {
   return course.chapters.find(
@@ -52,20 +91,27 @@ const isLessonComplete = computed(() => {
   if (!progress.value[chapter.value.number - 1]) {
     return false;
   }
-
-  if (!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
+  
+  if (
+    !progress.value[chapter.value.number - 1][
+    lesson.value.number - 1
+      ]
+  ) {
     return false;
   }
-
-  return progress.value[chapter.value.number - 1][lesson.value.number - 1];
+  
+  return progress.value[chapter.value.number - 1][
+  lesson.value.number - 1
+    ];
 });
 
 const toggleComplete = () => {
   if (!progress.value[chapter.value.number - 1]) {
     progress.value[chapter.value.number - 1] = [];
   }
-
-  progress.value[chapter.value.number - 1][lesson.value.number - 1] =
-    !isLessonComplete.value;
+  
+  progress.value[chapter.value.number - 1][
+  lesson.value.number - 1
+    ] = !isLessonComplete.value;
 };
 </script>
