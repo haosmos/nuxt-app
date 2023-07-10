adff3e355e0082ceb1884e6b11c09f997da47f7f
chaosmos <vitovich1987@gmail.com>
2023-07-10 16:26:24 +0300

    added landing page

--- components/UserCard.vue
+++ components/UserCard.vue
@@ -33,16 +33,15 @@ const logout = async () => {
   
   // The Nuxt Supabase auth *should* be doing this
   // for us, but it isn't for some reason.
-  // try {
-  //   await $fetch('/api/_supabase/session', {
-  //     method: 'POST',
-  //     body: { event: 'SIGNED_OUT', session: null },
-  //   });
-  //
-  //   user.value = null;
-  // } catch (error) {
-  //   console.error(error);
-  // }
+  try {
+    await $fetch('/api/_supabase/session', {
+      method: 'POST',
+      body: { event: 'SIGNED_OUT', session: null },
+    });
+    user.value = null;
+  } catch (e) {
+    console.error(error);
+  }
   
   await navigateTo('/login');
 };
